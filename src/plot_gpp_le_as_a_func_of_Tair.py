8c9082f0d83664a663f628e5dda34a76c5f20d77
mdekauwe <mdekauwe@gmail.com>
2017-12-18 11:33:18 +1100

    Added met file

--- src/plot_gpp_le_as_a_func_of_Tair.py
+++ src/plot_gpp_le_as_a_func_of_Tair.py
@@ -25,21 +25,68 @@ def main(flux_dir):
     if not os.path.exists(plot_dir):
         os.makedirs(plot_dir)
 
-    for fname in glob.glob(os.path.join(flux_dir, "*_flux.nc")):
-        (site, df, col_names) = open_file(fname)
-        print(col_names)
+    flux_files = sorted(glob.glob(os.path.join(flux_dir, "*_flux.nc")))
+    met_files = sorted(glob.glob(os.path.join(flux_dir, "*_met.nc")))
+
+    for flux_fn, met_fn in zip(flux_files, met_files):
+        (site, df_flux, df_met) = open_file(flux_fn, met_fn)
+
         print(site)
 
         sys.exit()
 
-def open_file(fname):
-    site = os.path.basename(fname).split("OzFlux")[0]
-    ds = xr.open_dataset(fname)
-    df = ds.to_dataframe()
-    df = ds.squeeze(dim=["x","y"], drop=True).to_dataframe()
-    col_names = list(df)
+def open_file(flux_fn, met_fn):
+    site = os.path.basename(flux_fn).split("OzFlux")[0]
+    ds = xr.open_dataset(flux_fn)
+    df_flx = ds.squeeze(dim=["x","y"], drop=True).to_dataframe()
+    ds = xr.open_dataset(met_fn)
+    df_met = ds.squeeze(dim=["x","y"], drop=True).to_dataframe()
 
-    return (site, df, col_names)
+    return (site, df_flx, df_met)
+
+def make_plot(plot_dir, site, df):
+
+    golden_mean = 0.6180339887498949
+    width = 6*2*(1/golden_mean)
+    height = width * golden_mean
+
+    fig = plt.figure(figsize=(width, height))
+    fig.subplots_adjust(hspace=0.1)
+    fig.subplots_adjust(wspace=0.05)
+    plt.rcParams['text.usetex'] = False
+    plt.rcParams['font.family'] = "sans-serif"
+    plt.rcParams['font.sans-serif'] = "Helvetica"
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['legend.fontsize'] = 14
+    plt.rcParams['xtick.labelsize'] = 14
+    plt.rcParams['ytick.labelsize'] = 14
+
+    almost_black = '#262626'
+    # change the tick colors also to the almost black
+    plt.rcParams['ytick.color'] = almost_black
+    plt.rcParams['xtick.color'] = almost_black
+
+    # change the text colors also to the almost black
+    plt.rcParams['text.color'] = almost_black
+
+    # Change the default axis colors from black to a slightly lighter black,
+    # and a little thinner (0.5 instead of 1)
+    plt.rcParams['axes.edgecolor'] = almost_black
+    plt.rcParams['axes.labelcolor'] = almost_black
+
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+
+    colour_list = ["#E69F00","#56B4E9", "#009E73", "#CC79A7"]
+
+    ax1.plot(df, df.GPP, ls=" ", marker="o",
+             color=colour_list[0], alpha=0.8)
+    ax2.plot(df, df.Qle, ls=" ", marker="o",
+             color=colour_list[1], alpha=0.8)
+
+    fig.savefig(os.path.join(plot_dir, "%s.pdf" % (site)),
+                bbox_inches='tight', pad_inches=0.1)
 
 
 if __name__ == "__main__":
