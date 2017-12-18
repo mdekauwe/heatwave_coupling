64a3bd2aa9e85a9353677da93c775ddd10d991e3
mdekauwe <mdekauwe@gmail.com>
2017-12-18 11:22:44 +1100

    Removed pet func

--- src/plot_gpp_le_as_a_func_of_Tair.py
+++ src/plot_gpp_le_as_a_func_of_Tair.py
@@ -8,7 +8,7 @@ That's all folks.
 """
 
 __author__ = "Martin De Kauwe"
-__version__ = "1.0 (08.11.2017)"
+__version__ = "1.0 (18.12.2017)"
 __email__ = "mdekauwe@gmail.com"
 
 import os
@@ -26,31 +26,21 @@ def main(flux_dir):
         os.makedirs(plot_dir)
 
     for fname in glob.glob(os.path.join(flux_dir, "*_flux.nc")):
-        site = os.path.basename(fname).split("OzFlux")[0]
-        ds = xr.open_dataset(fname)
-        df = ds.to_dataframe()
-
-        df = ds.squeeze(dim=["x","y"], drop=True).to_dataframe()
-
-        col_names = list(df)
+        (site, df, col_names) = open_file(fname)
         print(col_names)
         print(site)
 
         sys.exit()
 
-def calc_pet(Qh, Qle):
-    """ Energy only PET
+def open_file(fname):
+    site = os.path.basename(fname).split("OzFlux")[0]
+    ds = xr.open_dataset(fname)
+    df = ds.to_dataframe()
+    df = ds.squeeze(dim=["x","y"], drop=True).to_dataframe()
+    col_names = list(df)
 
-    This follows Milly and Dunne but I'm dropping the G bit for now
+    return (site, df, col_names)
 
-    Reference:
-    ==========
-    * Milly & Dunne (2016) Nature Climate Change, DOI: 10.1038/NCLIMATE3046
-
-    """
-    pet = 0.8 * (Qh + Qle)
-
-    return pet
 
 if __name__ == "__main__":
 
