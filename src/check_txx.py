4b403bfb4738e86ec5da7906618179d36f2d90cb
mdekauwe <mdekauwe@gmail.com>
2018-04-19 10:43:18 +1000

    Added script to get hottest day

---
+++ src/check_txx.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+"""
+For each of the Ozflux sites, plot LE & GPP as a function of Tair to probe
+decoupling
+
+That's all folks.
+"""
+
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (18.12.2017)"
+__email__ = "mdekauwe@gmail.com"
+
+import os
+import sys
+import glob
+import netCDF4 as nc
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import constants as c
+
+def main(flux_dir):
+
+    sites = ["AdelaideRiver","Calperum","CapeTribulation","CowBay",\
+             "CumberlandPlains","DalyPasture","DalyUncleared",\
+             "DryRiver","Emerald","Gingin","GreatWesternWoodlands",\
+             "HowardSprings","Otway","RedDirtMelonFarm","RiggsCreek",\
+             "Samford","SturtPlains","Tumbarumba","Whroo",\
+             "WombatStateForest","Yanco"]
+
+    pfts = ["SAV","SHB","TRF","TRF","EBF","GRA","SAV",\
+            "SAV","NA","EBF","EBF",\
+            "SAV","GRA","NA","GRA",\
+            "GRA","GRA","EBF","EBF",\
+            "EBF","GRA"]
+
+    d = dict(zip(sites, pfts))
+
+    plot_dir = "plots"
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+
+    flux_files = sorted(glob.glob(os.path.join(flux_dir, "*_flux.nc")))
+    met_files = sorted(glob.glob(os.path.join(flux_dir, "*_met.nc")))
+
+
+    for flux_fn, met_fn in zip(flux_files, met_files):
+        (site, df_flx, df_met) = open_file(flux_fn, met_fn)
+        #print(site)
+
+        # daylight hours
+        df_flx = df_flx.between_time("06:00", "20:00")
+        df_met = df_met.between_time("06:00", "20:00")
+
+        (df_flx, df_met) = mask_crap_days(df_flx, df_met)
+        df_met.Tair -= c.DEG_2_KELVIN
+
+        (Txx) = get_hottest_day(df_flx, df_met)
+
+        yrs = np.unique(df_flx.index.year).tolist()
+        yrs = ' '.join(str(e) for e in yrs)
+
+        print(site, d[site], round(Txx, 2), yrs)
+
+
+def get_hottest_day(df_flx, df_met):
+    df_dm = df_met.resample("D").mean()
+    df_df = df_flx.resample("D").mean()
+
+    Txx = df_dm.sort_values("Tair", ascending=False)[:1].Tair.values[0]
+
+    return(Txx)
+
+
+def mask_crap_days(df_flx, df_met):
+    # Mask crap stuff
+    df_flx.where(df_flx.Qle_qc == 1, inplace=True)
+    df_flx.where(df_met.Tair_qc == 1, inplace=True)
+
+    df_met.where(df_flx.Qle_qc == 1, inplace=True)
+    df_met.where(df_met.Tair_qc == 1, inplace=True)
+
+    # Mask dew
+    df_met.where(df_flx.Qle > 0., inplace=True)
+    df_flx.where(df_flx.Qle > 0., inplace=True)
+
+    df_met = df_met.reset_index()
+    df_met = df_met.set_index('time')
+    df_flx = df_flx.reset_index()
+    df_flx = df_flx.set_index('time')
+
+    return df_flx, df_met
+
+def open_file(flux_fn, met_fn):
+    site = os.path.basename(flux_fn).split("OzFlux")[0]
+    ds = xr.open_dataset(flux_fn)
+    df_flx = ds.squeeze(dim=["x","y"], drop=True).to_dataframe()
+    ds = xr.open_dataset(met_fn)
+    df_met = ds.squeeze(dim=["x","y"], drop=True).to_dataframe()
+
+    df_met = df_met.reset_index()
+    df_met = df_met.set_index('time')
+
+    df_flx = df_flx.reset_index()
+    df_flx = df_flx.set_index('time')
+
+    return (site, df_flx, df_met)
+
+if __name__ == "__main__":
+
+    flux_dir = "/Users/mdekauwe/research/OzFlux"
+    main(flux_dir)
