fde514c4702b3858620b89818e6333d7d5ae92ef
mdekauwe <mdekauwe@gmail.com>
2018-04-23 21:29:51 +1000

    Added GPP plots

---
+++ src/get_GPP_at_all_temps_above_Tthreh.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python
+
+"""
+For each of the OzFlux/FLUXNET2015 sites, figure out the TXx and save the
+Qle and bowen ratio for that day and the previous 4 days.
+
+That's all folks.
+"""
+
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (20.04.2017)"
+__email__ = "mdekauwe@gmail.com"
+
+import os
+import sys
+import glob
+import netCDF4 as nc
+import numpy as np
+import xarray as xr
+import pandas as pd
+
+import constants as c
+
+def main(flux_dir, ofname, oz_flux=True):
+
+    if oz_flux:
+        flux_files = sorted(glob.glob(os.path.join(flux_dir, "*_flux.nc")))
+        met_files = sorted(glob.glob(os.path.join(flux_dir, "*_met.nc")))
+    else:
+        flux_files = sorted(glob.glob(os.path.join(flux_dir, "*_Flux.nc")))
+        met_files = sorted(glob.glob(os.path.join(flux_dir, "*_Met.nc")))
+
+    output_dir = "outputs"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if oz_flux:
+        d = get_ozflux_pfts()
+
+    cols = ['site','pft','temp','GPP']
+    df = pd.DataFrame(columns=cols)
+    for flux_fn, met_fn in zip(flux_files, met_files):
+        (site, df_flx, df_met) = open_file(flux_fn, met_fn, oz_flux=oz_flux)
+
+
+        if d[site] == "EBF" or d[site] == "SAV" or d[site] == "TRF":
+            print(site, np.unique(df_met.index.year))
+            # daylight hours
+            df_flx = df_flx.between_time("06:00", "20:00")
+            df_met = df_met.between_time("06:00", "20:00")
+
+            (df_flx, df_met) = mask_crap_days(df_flx, df_met)
+            df_met.Tair -= c.DEG_2_KELVIN
+
+            (Tairs, GPPs) = get_all_events(df_flx, df_met)
+
+            if oz_flux:
+                pft = d[site]
+
+            lst = []
+            for i in range(len(Tairs)):
+                lst.append([site,d[site],Tairs[i],GPPs[i]])
+            dfx = pd.DataFrame(lst, columns=cols)
+            dfx = dfx.reindex(index=dfx.index[::-1]) # reverse the order hot to cool
+            df = df.append(dfx)
+
+    df.to_csv(os.path.join(output_dir, ofname), index=False)
+
+
+
+def get_all_events(df_flx, df_met):
+
+    # We need to figure out if it rained during our hot extreme as this
+    # would change the Qle in the way we're searching for!
+    diff = df_flx.index.minute[1] - df_flx.index.minute[0]
+    # Change GPP units
+    if diff == 0:
+        # hour gap i.e. Tumba
+        df_flx["GPP"] *= 12. * 0.000001 * 3600.0
+    else:
+        # 30 min gap
+        df_flx["GPP"] *= 12. * 0.000001 * 1800.0
+
+    df_dm = df_met.resample("D").max()
+    df_ds = df_met.resample("D").sum()
+    df_df = df_flx.resample("D").sum()
+
+    # We need to figure out if it rained during our hot extreme as this
+    # would change the Qle in the way we're searching for!
+    diff = df_met.index.minute[1] - df_met.index.minute[0]
+    if diff == 0:
+        # hour gap i.e. Tumba
+        rain = df_met.Rainf * 3600.0
+    else:
+        # 30 min gap
+        rain = df_met.Rainf * 1800.0
+    rain = rain.fillna(0.0)
+    rain = rain.resample("D").sum()
+
+
+    rain = rain.fillna(0.0)
+    rain = rain.resample("D").sum()
+
+    #
+    ## Get the TXx event
+    #
+    TXx = df_dm.sort_values("Tair", ascending=False)[:1].Tair.values[0]
+    TXx_idx = df_dm.sort_values("Tair", ascending=False)[:1].index.values[0]
+    TXx_idx_minus_four = TXx_idx - pd.Timedelta(3, unit='d')
+
+    (Tairs, GPPs) = get_values(df_dm, df_df, TXx_idx, TXx_idx_minus_four)
+
+    (Tairs, GPPs,
+     df_dm, df_df) = is_event_long_enough(df_dm, df_df, TXx_idx,
+                                           TXx_idx_minus_four, Tairs, GPPs,
+                                           rain)
+
+    if len(Tairs) < 4:
+        Tairs = np.array([np.nan,np.nan,np.nan,np.nan])
+        GPPs = np.array([np.nan,np.nan,np.nan,np.nan])
+
+
+    #
+    ## Get all the events other than the TXx that are > Tthresh
+    #
+
+    # Drop the hottest event as we've already got it
+    df_dm = df_dm[(df_dm.index < TXx_idx_minus_four) |
+                   (df_dm.index > TXx_idx)]
+    df_df = df_df[(df_df.index < TXx_idx_minus_four) |
+                   (df_df.index > TXx_idx)]
+    # Then get next TXx
+    TXx = df_dm.sort_values("Tair", ascending=False)[:1].Tair.values[0]
+
+    while TXx > 37.0:
+
+        # Then get next TXx
+        TXx = df_dm.sort_values("Tair", ascending=False)[:1].Tair.values[0]
+        TXx_idx = df_dm.sort_values("Tair", ascending=False)[:1].index.values[0]
+        TXx_idx_minus_four= TXx_idx - pd.Timedelta(3, unit='d')
+
+        (Tairsx, GPPsx) = get_values(df_dm, df_df, TXx_idx, TXx_idx_minus_four)
+
+        (Tairsx, GPPsx,
+         df_dm, df_df) = is_event_long_enough(df_dm, df_df, TXx_idx,
+                                                  TXx_idx_minus_four,
+                                                  Tairsx, GPPsx, rain)
+
+        Tairsx = Tairsx[~np.isnan(Tairsx)]
+        GPPsx = GPPsx[~np.isnan(GPPsx)]
+
+        if len(Tairsx) == 4:
+            Tairs = np.append(Tairs, Tairsx)
+            GPPs = np.append(GPPs, GPPsx)
+
+        # Drop this event
+        df_dm = df_dm[(df_dm.index < TXx_idx_minus_four) |
+                       (df_dm.index > TXx_idx)]
+        df_df = df_df[(df_df.index < TXx_idx_minus_four) |
+                       (df_df.index > TXx_idx)]
+        # Then get next TXx
+        TXx = df_dm.sort_values("Tair",
+                                ascending=False)[:1].Tair.values[0]
+
+    Tairs = Tairs[~np.isnan(Tairs)]
+    GPPs = GPPs[~np.isnan(GPPs)]
+
+    if len(Tairs) < 4:
+        Tairs = np.array([np.nan,np.nan,np.nan,np.nan])
+        GPPs = np.array([np.nan,np.nan,np.nan,np.nan])
+
+    return(Tairs, GPPs)
+
+def get_values(df_dm, df_df, TXx_idx, TXx_idx_minus_four):
+
+    Tairs = df_dm[(df_dm.index >= TXx_idx_minus_four) &
+                  (df_dm.index <= TXx_idx)].Tair.values
+    GPPs = df_df[(df_dm.index >= TXx_idx_minus_four) &
+                 (df_dm.index <= TXx_idx)].GPP.values
+
+    return (Tairs, GPPs)
+
+def is_event_long_enough(df_dm, df_df, TXx_idx, TXx_idx_minus_four,
+                         Tairs, GPPs, rain):
+
+    while len(Tairs) != 4:
+
+        # Drop this event as it wasn't long enough
+        df_dm = df_dm[(df_dm.index < TXx_idx_minus_four) |
+                       (df_dm.index > TXx_idx)]
+        df_df = df_df[(df_df.index < TXx_idx_minus_four) |
+                       (df_df.index > TXx_idx)]
+
+        TXx = df_dm.sort_values("Tair", ascending=False)[:1].Tair.values[0]
+        TXx_idx = df_dm.sort_values("Tair",
+                                    ascending=False)[:1].index.values[0]
+        TXx_idx_minus_four= TXx_idx - pd.Timedelta(3, unit='d')
+
+        (Tairs, GPPs) = get_values(df_dm, df_df, TXx_idx, TXx_idx_minus_four)
+
+        (Tairs, GPPs,
+         df_dm, df_df) = check_for_rain(rain, TXx_idx_minus_four,
+                                        TXx_idx, df_dm, df_df,
+                                        Tairs, GPPs)
+
+        if len(df_dm) <= 4:
+            Tairs = np.array([np.nan,np.nan,np.nan,np.nan])
+            GPPs = np.array([np.nan,np.nan,np.nan,np.nan])
+            break
+
+    return (Tairs, GPPs, df_dm, df_df)
+
+
+
+def check_for_rain(rain, TXx_idx_minus_four, TXx_idx, df_dm, df_df,
+                   Tairs, GPPs):
+
+    threshold = 0.2 # mm d-1; arbitary, we can refine.
+    total_rain = np.sum(rain[(rain.index >= TXx_idx_minus_four) &
+                             (rain.index <= TXx_idx)].values)
+
+    while total_rain > threshold or len(Tairs) != 4:
+
+        # Drop this event as there was some rain or we didn't get 5 good QA days
+        df_dm = df_dm[(df_dm.index < TXx_idx_minus_four) |
+                       (df_dm.index > TXx_idx)]
+        df_df = df_df[(df_df.index < TXx_idx_minus_four) |
+                       (df_df.index > TXx_idx)]
+
+        TXx = df_dm.sort_values("Tair", ascending=False)[:1].Tair.values[0]
+        TXx_idx = df_dm.sort_values("Tair",
+                                    ascending=False)[:1].index.values[0]
+        TXx_idx_minus_four = TXx_idx - pd.Timedelta(3, unit='d')
+
+        (Tairs, GPPs) = get_values(df_dm, df_df, TXx_idx, TXx_idx_minus_four)
+
+        total_rain = np.sum(rain[(rain.index >= TXx_idx_minus_four) &
+                                 (rain.index <= TXx_idx)].values)
+
+        if len(df_dm) <= 4:
+            Tairs = np.array([np.nan,np.nan,np.nan,np.nan])
+            GPPs = np.array([np.nan,np.nan,np.nan,np.nan])
+            break
+
+    return (Tairs, GPPs, df_dm, df_df)
+
+
+
+def get_ozflux_pfts():
+
+    sites = ["AdelaideRiver","Calperum","CapeTribulation","CowBay",\
+             "CumberlandPlains","DalyPasture","DalyUncleared",\
+             "DryRiver","Emerald","Gingin","GreatWesternWoodlands",\
+             "HowardSprings","Otway","RedDirtMelonFarm","RiggsCreek",\
+             "Samford","SturtPlains","Tumbarumba","Whroo",\
+             "WombatStateForest","Yanco"]
+
+    pfts = ["SAV","EBF","TRF","TRF","EBF","GRA","SAV",\
+            "SAV","NA","EBF","EBF",\
+            "SAV","GRA","NA","GRA",\
+            "GRA","GRA","EBF","EBF",\
+            "EBF","GRA"]
+
+    d = dict(zip(sites, pfts))
+
+    return d
+
+def mask_crap_days(df_flx, df_met):
+    """ Mask bad QA, i.e. drop any data where Qle, Qa, Tair and Rain are flagged
+    as being of poor quality"""
+
+    thresh = 0.1 / 1800. # 0.1 mm/hlfhr -> mm/s
+    #df_flx.where(df_flx.Qle_qc == 1, inplace=True)
+    #df_flx.where(df_flx.Qh_qc == 1, inplace=True)
+    df_flx.where(df_flx.GPP_qc == 1, inplace=True)
+    df_flx.where(df_met.Tair_qc == 1, inplace=True)
+
+    #df_met.where(df_flx.Qle_qc == 1, inplace=True)
+    #df_met.where(df_flx.Qh_qc == 1, inplace=True)
+    df_met.where(df_flx.GPP_qc == 1, inplace=True)
+    df_met.where(df_met.Tair_qc == 1, inplace=True)
+
+
+    # Mask dew
+    #df_met.where(df_flx.Qle > 0., inplace=True)
+    #df_flx.where(df_flx.Qle > 0., inplace=True)
+
+    df_met = df_met.reset_index()
+    df_met = df_met.set_index('time')
+    df_flx = df_flx.reset_index()
+    df_flx = df_flx.set_index('time')
+
+    return df_flx, df_met
+
+def open_file(flux_fn, met_fn, oz_flux=True):
+    site = os.path.basename(flux_fn).split("OzFlux")[0]
+
+    ds = xr.open_dataset(flux_fn)
+    df_flx = ds.squeeze(dim=["x","y"], drop=True).to_dataframe()
+    df_flx = df_flx.reset_index()
+    df_flx = df_flx.set_index('time')
+
+    ds = xr.open_dataset(met_fn)
+    df_met = ds.squeeze(dim=["x","y"], drop=True).to_dataframe()
+    df_met = df_met.reset_index()
+    df_met = df_met.set_index('time')
+
+    return (site, df_flx, df_met)
+
+if __name__ == "__main__":
+
+    oz_flux = True
+    if oz_flux:
+        flux_dir = "/Users/mdekauwe/research/OzFlux"
+        ofname = "ozflux_all_events_GPP.csv"
+    else:
+        flux_dir = "/srv/ccrc/data04/z3509830/Fluxnet_data/Data_for_Jiafu/Daily/Nc_files"
+        flux_dir = "/Users/mdekauwe/Desktop/test"
+        ofname = "fluxnet2015.csv"
+    main(flux_dir, ofname, oz_flux=oz_flux)
---
+++ src/plot_GPP_at_all_events_above_Tthreh.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+"""
+For each of the OzFlux/FLUXNET2015 sites, plot the TXx and T-4 days
+Qle and bowen ratio
+
+That's all folks.
+"""
+
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (20.04.2018)"
+__email__ = "mdekauwe@gmail.com"
+
+import os
+import sys
+import glob
+import netCDF4 as nc
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import constants as c
+
+def main(fname):
+
+    plot_dir = "plots"
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+
+    df = pd.read_csv(fname)
+    df = df[df.pft == "EBF"]
+
+    #width  = 12.0
+    #height = width / 1.618
+    #print(width, height)
+    #sys.exit()
+    width = 14
+    height = 5
+    fig = plt.figure(figsize=(width, height))
+    fig.subplots_adjust(hspace=0.1)
+    fig.subplots_adjust(wspace=0.1)
+    plt.rcParams['text.usetex'] = False
+    plt.rcParams['font.family'] = "sans-serif"
+    plt.rcParams['font.sans-serif'] = "Helvetica"
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['legend.fontsize'] = 10
+    plt.rcParams['xtick.labelsize'] = 14
+    plt.rcParams['ytick.labelsize'] = 14
+
+
+    count = 0
+    sites = np.unique(df.site)
+    for site in sites:
+
+        ax = fig.add_subplot(2,4,1+count)
+
+        df_site = df[df.site == site]
+        events = int(len(df_site)/4)
+
+        cnt = 0
+        for e in range(0, events):
+
+            from scipy import stats
+            x = df_site["temp"][cnt:cnt+4]
+            y = df_site["GPP"][cnt:cnt+4]
+            slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+            #print(site, slope, p_value)
+            if slope < 0.0 and p_value <= 0.05:
+                ax.plot(df_site["temp"][cnt:cnt+4], df_site["GPP"][cnt:cnt+4],
+                        label=site, ls="-", marker="o", zorder=100)
+            elif slope < 0.0 and p_value > 0.05:
+                ax.plot(df_site["temp"][cnt:cnt+4], df_site["GPP"][cnt:cnt+4],
+                        label=site, ls="-", marker="o", color="lightgrey",
+                        zorder=1)
+            cnt += 4
+
+        if count == 0:
+            ax.set_ylabel("GPP (g C m$^{-2}$ d$^{-1}$)", position=(0.5, 0.0))
+        if count == 5:
+            ax.set_xlabel('Temperature ($^\circ$C)', position=(1.0, 0.5))
+
+        if count < 4:
+            plt.setp(ax.get_xticklabels(), visible=False)
+
+        if count != 0 and count != 4:
+            plt.setp(ax.get_yticklabels(), visible=False)
+
+        props = dict(boxstyle='round', facecolor='white', alpha=1.0,
+                     ec="white")
+        ax.text(0.04, 0.95, site,
+                transform=ax.transAxes, fontsize=14, verticalalignment='top',
+                bbox=props)
+
+        ax.set_ylim(0, 10)
+        ax.set_xlim(15, 50)
+        count += 1
+
+    #fig.savefig("/Users/mdekauwe/Desktop/Qle_bowen_Txx_minus5.pdf",
+    #            bbox_inches='tight', pad_inches=0.1)
+    fig.savefig("/Users/mdekauwe/Desktop/all_events_GPP.pdf",
+                bbox_inches='tight', pad_inches=0.1)
+    #plt.show()
+
+if __name__ == "__main__":
+
+    data_dir = "outputs/"
+    fname = "ozflux_all_events_GPP.csv"
+    fname = os.path.join(data_dir, fname)
+    main(fname)
